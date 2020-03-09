387bb13586389e4a0de1ac1355080d2f20abd6e2
andiaspin <andiaspin3@gmail.com>
2020-03-09 19:16:16 +0800

    Barcode Scanner

---
+++ app/src/main/java/id/ndiappink/scannerbarcode/MainActivity.java
@@ -0,0 +1,77 @@
+package id.ndiappink.scannerbarcode;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btnscan = (Button)findViewById(R.id.buttonScan);
+        btnscan.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if (result != null){
+            if (result.getContents() == null){
+                Toast.makeText(this,"Result Not Found", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                AlertDialog.Builder alertdialogbuilder = new AlertDialog.Builder(this);
+                alertdialogbuilder.setMessage(result.getContents()+"\n\nScan Again ?");
+                alertdialogbuilder.setTitle("Result Scanned");
+                alertdialogbuilder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        scanow();
+                    }
+                });
+                alertdialogbuilder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+                AlertDialog alertDialog = alertdialogbuilder.create();
+                alertDialog.show();
+            }
+        }
+        else{
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    public void scanow(){
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setCaptureActivity(Portrait.class);
+        integrator.setOrientationLocked(false);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+        integrator.setPrompt("Scan Your Barcode");
+        integrator.initiateScan();
+    }
+
+    @Override
+    public void onClick(View v) {
+        scanow();
+    }
+}
---
+++ app/src/main/java/id/ndiappink/scannerbarcode/Portrait.java
@@ -0,0 +1,9 @@
+package id.ndiappink.scannerbarcode;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+/**
+ * Created by NDIAPPINK on 4/9/2018.
+ */
+public class Portrait extends CaptureActivity {
+}
